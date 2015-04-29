8af8180e5ec1a416b2a9178a5ddfcc704dbeb5d0
krithi-shareth <krithi.shareth@gmail.com>
2015-04-29 13:35:08 -0500

    Update FileAttributes.java

--- FileAttributes.java
+++ FileAttributes.java
@@ -1,23 +1,27 @@
 import java.util.ArrayList;
+import java.util.Set;
 
 
 public class FileAttributes {
-
+	
+	int currentReqID;
 	String filename;
 	int nodeid;
 	int verNum;
 	int RU;
 	int M;
-	ArrayList<Integer> P;
-	ArrayList<Integer> Q;
+	ArrayList<FileAttributes> P;
+	ArrayList<FileAttributes> Q;
     int locktype; // 0 - read 1 - write 
     int DS;
+    Set<String> requestNodeList;
     
     
     
 
-    public  FileAttributes( String filename, int nodeid, int verNum, int RU, int M, ArrayList<Integer> P, ArrayList<Integer> Q, int locktype, int DS)
+    public  FileAttributes( int currentReqID, String filename, int nodeid, int verNum, int RU, int M, ArrayList<FileAttributes> P, ArrayList<FileAttributes> Q, int locktype, int DS, Set<String> requestNodeList)
     {
+       this.currentReqID=currentReqID;
        this.filename=filename;
        this.nodeid=nodeid;
        this.locktype=locktype;
@@ -27,6 +31,8 @@ public class FileAttributes {
        this.P=P;
        this.Q=Q;
        this.DS=DS;
+       this.requestNodeList=requestNodeList;
     }
+    
 	
 }
