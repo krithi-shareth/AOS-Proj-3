fb7e9bd92d69dc847a59f2857534451c28684b08
krithi-shareth <krithi.shareth@gmail.com>
2015-04-29 13:37:50 -0500

    Update MessageStruct.java

--- MessageStruct.java
+++ MessageStruct.java
@@ -5,24 +5,38 @@ class MessageStruct implements Serializable
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	String reqID;
-	int msgType; //0 - req 1-response 3-abort
-    int nodeid ;
-    int locktype; // 0 - read 1 - write
+	int reqID;  
+	int msgType; //0 - req 1-response 2-abort 3-release 4-sendfile 5-recvfile
+    int nodeid ; // current node id
+    int locktype; // 0 - read 1 - write 9-free
     String filename;
     int verNum;
+    int RU;
+    FileAttributes faobj;
     
     
 
-    public  MessageStruct( String reqID, int msgType, int nodeid, int locktype, String filename, int verNum)
+    public  MessageStruct( int reqID, int msgType, int nodeid, int locktype,String Filename, FileAttributes faobj, int verNum, int RU)
     {
        this.reqID=reqID;
        this.msgType=msgType;
        this.nodeid=nodeid;
        this.locktype=locktype;
-       this.filename=filename;
+       this.faobj=faobj;
+       this.filename=Filename;
        this.verNum=verNum;
+       this.RU=RU;
     }
-
+    
+    //for abort message
+    public  MessageStruct( int reqID, int msgType, int nodeid, int locktype,String Filename)
+    {
+       this.reqID=reqID;
+       this.msgType=msgType;
+       this.nodeid=nodeid;
+       this.locktype=locktype;
+       this.filename=Filename;
+       
+    }
    
 }
